86be2c5bcb01d76c91f6eb65b5db8eb95fb8f240
Jongjunp <pjjkey@kaist.ac.kr>
2023-12-22 23:45:06 +0900

    initial commit

---
+++ CMakeFiles/obj-viewer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obj-viewer.dir/obj-viewer.cpp.o"
+  "CMakeFiles/obj-viewer.dir/obj-viewer.cpp.o.d"
+  "obj-viewer"
+  "obj-viewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/obj-viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/obj-viewer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for obj-viewer.
+# This may be replaced when dependencies are built.
